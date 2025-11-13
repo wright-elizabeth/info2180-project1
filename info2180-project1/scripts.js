9e20b7be8a902fa0ad7e5f50812dcda629b1bab3
wright-elizabeth <liz.wright5665@gmail.com>
2025-11-13 10:20:46 -0500

    Finalized project styling, images, and interactivity

--- info2180-project1/scripts.js
+++ info2180-project1/scripts.js
@@ -1,1 +1,16 @@
-/* Add your JavaScript to this file */
+window.onload = function () {
+  const form = document.querySelector(".newsletter form");
+  const emailInput = document.querySelector("#email");
+  const messageDiv = document.querySelector(".message");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // prevent page reload
+    const email = emailInput.value.trim();
+
+    if (email === "") {
+      messageDiv.textContent = "Please enter a valid email address.";
+    } else {
+      messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+    }
+  });
+};
